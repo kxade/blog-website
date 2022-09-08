cf22324a7df7524754a4b6c5f14bcb2e0cd48d7a
kxade <ndibragim@gmail.com>
2022-09-08 03:42:55 +0300

    add register

--- women/utils.py
+++ women/utils.py
@@ -5,7 +5,7 @@ from django.db.models.aggregates import *
 menu = [{'title': 'О сайте', 'url_name': 'about'}, 
         {'title': 'Добавить статью', 'url_name': 'add_page'},
         {'title': 'Обратная связь', 'url_name': 'contact'},
-        {'title': 'Войти', 'url_name': 'login'}]
+        ]
 
 
 class DataMixin:
@@ -18,8 +18,7 @@ class DataMixin:
         user_menu = menu.copy()
         if not self.request.user.is_authenticated:
             user_menu.pop(1)
-        else:
-            user_menu.pop(3)
+
         context['menu'] = user_menu
         context['cats'] = cats
         if 'cat_selected' not in context:
